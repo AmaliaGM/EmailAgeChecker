7ae4434ad729a65a1413ae9e4e27f39147d79f39
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-01 15:07:00 -0300

    linters

--- age_checker.rb
+++ age_checker.rb
@@ -1,11 +1,11 @@
-puts "What is your name?"
+puts 'What is your name?'
 name = gets
 
-puts "What is your age?"
+puts 'What is your age?'
 age = gets
 
-if age.to_i>=16
-    puts "#{name} You are 16 or older!"
+if age.to_i >= 16
+  puts "#{name} You are 16 or older!"
 else
-    puts "#{name} You are not over the age of 16!"
+  puts "#{name} You are not over the age of 16!"
 end
